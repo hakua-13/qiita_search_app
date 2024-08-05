913b4f1aa8ae87b2ca9c79e889aa74818f026a92
hakua <hakua1452@gmail.com>
2024-08-06 00:48:19 +0900

    不要なimportを削除

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:qiita_search/models/article.dart';
-import 'package:qiita_search/models/user.dart';
 import 'package:qiita_search/widgets/article_container.dart';
 
 class SearchScreen extends StatefulWidget {
